879e8c26e3e499120eb80ffe5a56403f0d703a82
madisonewebb <madisonewebb28@gmail.com>
2025-03-08 20:18:57 -0800

    Minified the JSON object

--- myapp/app.js
+++ myapp/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.set('json spaces', 0);
 
 app.get('/', function (req, res) {
   res.json({ message: 'My name is Madison' , timestamp: (new Date()).toISOString() })
