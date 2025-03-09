556ac459dd8fcceabb46c3fb8602845f67107955
madisonewebb <madisonewebb28@gmail.com>
2025-03-08 18:45:11 -0800

    Created single endpoint web application that returns JSON object of my name

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.json({ message: 'My name is Madison' })
 });
+
 app.listen(3000, function () {
   console.log('Test app listening on port 3000!');
 });
